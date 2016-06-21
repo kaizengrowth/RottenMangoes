910428630e064792844cc1eaf3b63abe29ab4416
kaizencole <papertigerlily@gmail.com>
2016-06-21 19:37:38 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenMangoes::Application.config.secret_key_base = '593423d943268be84460f63cf51cf1fb81de92b2cf05154a45aeca83f728852986e89b1d4966b528babe7b0336f97c386a5e92b886ccee419af9d98c0e4d35c5'
---
+++ db/migrate/20160621191457_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :director
+      t.integer :runtime_in_minutes
+      t.text :description
+      t.string :poster_image
+
+      t.timestamps
+    end
+  end
+end
